fa0335d1ba04b7aa0665ee2bd2a559d4b60e934a
LukeMcCann <developer-lukemccann@outlook.com>
2022-03-27 20:23:42 +0100

    Add basic streaming and chunking

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Base Server
+# VideoStreaming
 
-Basic Docker container for quickly spinning up a Node API project.
+Experimental video streaming service. This is broken down into two very basic Micsorservices, to be used as the basis of a small streaming service. This is no production ready and would need many tweaks, this is only a basic prototype consisting of two services (a Client and an API).
 
 # Usage
 
--- api/src/index.mjs
+++ api/src/index.mjs
@@ -1,11 +1,38 @@
 'use strict';
 
 import express from 'express';
+import path from 'path';
+import { statSync, createReadStream } from 'fs';
 
 const app = express();
+const __dirname = path.resolve();
 
 app.get('/video', (req, res) => {
-    res.send('Hello World!')
+    const range = req.headers.range;
+    if (!range) {
+        res.status(400).send('Range header is requires');
+    }
+
+    const videoPath = path.join(__dirname, "/data/videos/test.mp4");
+    const videoSize = statSync(videoPath).size;
+
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+    const contentLength = end - start + 1;
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    };
+
+    res.writeHead(206, headers);
+
+    const videoStream = createReadStream(videoPath, { start, end });
+
+    videoStream.pipe(res);
 });
 
 app.listen(3000, () => {
